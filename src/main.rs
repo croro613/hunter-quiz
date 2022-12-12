d6d60ed704ee193830b1c7186467f5e56a0f96a1
Yuta akiyama <croro613@YutanoMacBook-Pro.local>
2022-12-12 21:54:06 +0900

    release

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/add_quiz")]
+    AddQuiz,
+    #[at("/")]
+    Home,
+    #[at("/secure")]
+    Secure,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
 
 #[function_component(App)]
 fn app() -> Html {
